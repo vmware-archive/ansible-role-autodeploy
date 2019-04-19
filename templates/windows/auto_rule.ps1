a49fbcb71053670db9f78d959f194c0a2ce0b2cc
John 'Warthog9' Hawley <jhawley@vmware.com>
2019-04-19 16:40:38 -0700

    Changing the license from Apache-2.0 to Apache-2.0 or GPLv3
    The change is specifically to add an additional license under which the
    code will be licensed (going forward it will be dual licensed),
    specifically to add GPLv3 in addition to it's existing license.  This
    change is being done to better align the code with upstream Ansible.
    
    Signed-off-by: John 'Warthog9' Hawley <jhawley@vmware.com>
    Signed-off-by: John 'Warthog9' Hawley <warthog9@eaglescrag.net>

--- templates/windows/auto_rule.ps1
+++ templates/windows/auto_rule.ps1
@@ -1,18 +1,19 @@
 {#
-   Copyright 2015 VMware, Inc.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Copyright 2015 VMware, Inc.  All rights reserved.
 
-       http://www.apache.org/licenses/LICENSE-2.0
+  SPDX-License-Identifier: Apache-2.0 OR GPL-3.0-only
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-#} 
+  This code is Dual Licensed Apache License 2.0 or GPLv3
+
+  You may obtain a copy of the License(s) at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+      or
+
+      https://www.gnu.org/licenses/gpl-3.0.en.html
+#}
 $productName = "vSphere PowerCLI"
 $productShortName = "PowerCLI"
 $windowTitle = "VMware $productName"
